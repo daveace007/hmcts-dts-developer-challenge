f5c46458b0347cd941392d888594451f410ef3c9
candidate <candidate@mail.com>
2025-06-26 06:59:03 +0100

    subtle code format change

--- task-manager/src/App.tsx
+++ task-manager/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Header } from "./layouts/HeaderAndFooter/Header"
 import { Footer } from "./layouts/HeaderAndFooter/Footer"
 import { TasksPage } from './layouts/TasksPage/TasksPage';
+
 export const App = () => {
   return (
     <div className='d-flex flex-column min-vh-100 bg-transparent'>
