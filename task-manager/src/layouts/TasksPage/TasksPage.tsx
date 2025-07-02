640f7a50df1702e7a32ed3b7aa436e053e8a4e1c
candidate <candidate@mail.com>
2025-07-02 03:37:20 +0100

    create functionalities to request and display a group of  tasks via an external api

--- task-manager/src/layouts/TasksPage/TasksPage.tsx
+++ task-manager/src/layouts/TasksPage/TasksPage.tsx
@@ -3,8 +3,73 @@
 import { SearchBar } from "./components/SearchBar";
 import { TaskItem } from "./components/TaskItem";
 import { PaginationBar } from "./components/PaginationBar";
+import { useState, useEffect } from "react";
+import TaskModel from "../../model/TaskModel";
+import * as Routes from "../../Routes"
 
 export const TasksPage = () => {
+
+    const [tasks, setTasks] = useState<TaskModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() =>{
+        const fetchTasks = async ()=>{
+            const url:string = `${Routes.BASE_URL}?page=0&size=10`;
+
+            const response = await fetch(url);
+
+            if (!response.ok){
+                throw new Error("Something went wrong");
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson.content;
+
+            const loadedTasks : TaskModel[] = [];
+
+            
+            for (const key in responseData){
+                loadedTasks.push(
+                    {
+                        id: responseData[key].id,
+                        title: responseData[key].title,
+                        description: responseData[key].description,
+                        status: responseData[key].status,
+                        dueDateTime: responseData[key].dueDateTime
+                    }
+                )
+            }
+            setTasks(loadedTasks);
+            setIsLoading(false);
+            console.log(loadedTasks);
+
+        }
+
+        fetchTasks().catch((error:any)=>{
+            setIsLoading(false);
+            setHttpError(error.message);
+        });
+
+    },[]);
+
+    if (isLoading){
+        return (
+            <div className='container m-5'>
+                <p>Loading..</p>
+            </div>
+        )
+    }
+
+    if (httpError){
+        return (
+            <div className='container m-5'>
+                <p>{httpError}</p>
+            </div>
+        )
+    }
+
     return (
         <div className='d-flex flex-column h-100'>
             <div style={{position:'sticky', top:'19vh', zIndex:1020}}>
@@ -12,22 +77,9 @@ export const TasksPage = () => {
             </div>
             <div className="flex-grow-1 overflow-auto">
                 <div className='p-3'>
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
-                    <TaskItem />
+                    {
+                        tasks.map(task => <TaskItem task={task} key={task.id}/>)
+                    }
                 </div>
             </div>
              <div className='d-flex w-100 p-3 bg-dark align-items-center justify-content-center' style={{position:'sticky', bottom:'6vh',zIndex:1020}}>
--- task-manager/src/layouts/TasksPage/components/TaskItem.tsx
+++ task-manager/src/layouts/TasksPage/components/TaskItem.tsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 import * as Routes from "../../../Routes"
+import TaskModel from "../../../model/TaskModel";
 
-export const TaskItem = () => {
+export const TaskItem:React.FC<{task:TaskModel}> = (props) => {
 
     return (
         <div className='card d-flex flex-row bg-light text-dark p-2 mb-1'>
 
             <span className="rounded-circle bg-dark text-light d-flex justify-content-center align-items-center">
                 <span className="rounded-circle bg-light text-dark d-flex justify-content-center align-items-center" style={{ width: '2em', height: '2em', fontSize: '1em' }}>
-                    {1}
+                    {props.task.id}
                 </span>
             </span>
-            <span className='flex-grow-1 text-center'>Dev Ops Task</span>
+            <span className='flex-grow-1 text-center'>
+                {props.task.title}
+            </span>
             <div className='d-flex flex-row gap-1'>
-                <Link className='btn btn-primary' type='button' to={Routes.CURRENT_TASK_PAGE}>More</Link>
+                <Link className='btn btn-primary' type='button' to={{pathname:Routes.CURRENT_TASK_PAGE, state:{props}}}>More</Link>
                 <button className='btn btn-danger' type='button'>Delete</button>
             </div>
 
